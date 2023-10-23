0379cc63d954d4fa82a0dd7381dcfd2a569f552a
Andres Reynoso <u202010196@upc.edu.pe>
2023-10-22 19:04:28 -0500

    Added layout segment

---
+++ src/assets/layout/pages/dashboard-consumer.component.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="dashboard">
+        <Sidebar></Sidebar>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from '../components/sidebar-consumer.component.vue'
+export default {
+    name: "DashBoard-consumer",
+    components: {Sidebar}
+}
+</script>
+
+<style scoped>
+    .dashboard{
+        display: grid;
+        grid-template-columns: 1fr 5fr;
+    }
+
+    .content{
+        width: 79vw;
+        height: 94vh;
+        text-align: center;
+        background-color: rgba(233, 65, 82, 1);
+        border-radius:10px;
+        margin: 5px 18vw;
+    }
+</style>
